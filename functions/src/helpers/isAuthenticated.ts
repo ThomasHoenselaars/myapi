54f913b9916c4d44db0e48a4d0d949c8ef7bf469
Thomas Hoenselaars <thomas@angistudio.com>
2021-04-19 16:02:10 +0200

    update to working token

--- functions/src/helpers/isAuthenticated.ts
+++ functions/src/helpers/isAuthenticated.ts
@@ -3,15 +3,16 @@ const admin = require('firebase-admin');
 
 export const isAuthenticated: ExpressRoute = async (req, res, next) => {
   try {
-    const decodedToken = await admin.auth().verifyIdToken(req.headers.authorization);
-    console.log('Token', JSON.stringify(decodedToken))
+    const decodedToken = await admin.auth().verifyIdToken(req.headers.authorization?.split('Bearer ')[1]);
 
     res.locals = {
       ...res.locals,
       uid: decodedToken.uid,
     }
 
-    return next();
+    next();
+
+    return;
   } catch (err) {
     console.error(`${err.code} - ${err.message}`)
     return res.status(401).send({ message: err })
